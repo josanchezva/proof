c7d8c302bf94e94f672a8edbd6bd622fad3deef7
josanchezva <josanchezva@unal.edu.co>
2020-06-05 23:06:05 -0500

    master

--- src/proof/Proof.java
+++ src/proof/Proof.java
@@ -27,7 +27,7 @@ public class Proof extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Branc 2!");
+                System.out.println("master!");
             }
         });
         
